5f6dbbac7a8bb4f6561685c7ea654c76fe05f7ab
mickche <174184243+mickche@users.noreply.github.com>
2026-02-21 14:53:06 +0200

    add token to example

--- Finbot_example/bot.py
+++ Finbot_example/bot.py
@@ -1,9 +1,15 @@
 
 from aiogram import Bot, Dispatcher
 from routers import root_router
+from pathlib import Path
+from dotenv import dotenv_values
 
 
-bot = Bot(token="")
+env_path = Path(__file__).parent.parent / ".env.local"
+
+config = dotenv_values(env_path)
+
+bot = Bot(token=config["TOKEN"])
 dp = Dispatcher()
 
 dp.include_router(root_router)
