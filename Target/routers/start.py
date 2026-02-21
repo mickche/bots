ce582981f809e4a25c1fb1d999ad00b8671acdb5
mickche <174184243+mickche@users.noreply.github.com>
2026-02-21 12:21:44 +0200

    add callback data

--- Target/routers/start.py
+++ Target/routers/start.py
@@ -10,8 +10,8 @@ start_router = Router()
 @start_router.message(Command("start"))
 async def handle_start(message: Message):
     builder = InlineKeyboardBuilder()
-    builder.button(text="Калькулятор", url="https://aiogram.dev/")
-    builder.button(text="Таймер", url="https://github.com/")
+    builder.button(text="Калькулятор", callback_data="start_router")
+    builder.button(text="Таймер", callback_data="write_router")
 
     await message.reply(
         text="Привіт я КалкТаймер бот. ",
